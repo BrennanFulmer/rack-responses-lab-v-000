c64044272e996c41a36ec162707f4765851f8561
Brennan Fulmer <brennan.flmr@gmail.com>
2018-07-04 17:40:06 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
  
     current_time = Time.new
     
-    if current_time.hour < 12
+    if current_time.hour < 11
       resp.write "Good Afternoon!"
     else
       resp.write "Good Morning!"
