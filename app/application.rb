c3bd1160cb5eccb3a10755c98fe5a14e095f4e2f
Brennan Fulmer <brennan.flmr@gmail.com>
2018-07-04 17:35:35 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,6 @@ class Application
     resp = Rack::Response.new
  
     current_time = Time.new
-    meridian = current_time.strftime('%p')
     
     if meridian.downcase == 'pm'
       resp.write "Good Morning!"
